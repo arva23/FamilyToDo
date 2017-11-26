0ce18928b7d4325bb40cf8f30c60a987a049c10e
arva23 <arva23@gmail.com>
2017-11-26 09:38:18 +0100

    init blank project

---
+++ src/main/java/hu/elte/alkfejl/familytodolist/FamilytodolistApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.familytodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilytodolistApplication.class, args);
+	}
+}
